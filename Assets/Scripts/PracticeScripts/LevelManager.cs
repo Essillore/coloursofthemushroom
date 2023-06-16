200f8aa0bcfe1315800109f7e288d5c5ca3a90ba
Essillore <riikka.pehkonen@me.com>
2023-06-16 13:27:22 +0300

    Commit 120623/ Level manager code cleanup

--- Assets/Scripts/PracticeScripts/LevelManager.cs
+++ Assets/Scripts/PracticeScripts/LevelManager.cs
@@ -5,23 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-
     public PauseScript pause;
-   // public Animator fadeScreen;
+    // public Animator fadeScreen;
     public float transitionTime = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-      //  pause = GetComponent<PauseScript>();
-      //  fadeScreen = GameObject.Find("FadeScreen").GetComponent<Animator>();
+        //  pause = GetComponent<PauseScript>();
+        //  fadeScreen = GameObject.Find("FadeScreen").GetComponent<Animator>();
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void ChangeLevel(int levelNumber)
